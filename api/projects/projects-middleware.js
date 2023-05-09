d442d6695a7dc81f7932899d32753957709e8284
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-05-09 13:47:22 -0400

    projects completed

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,71 @@
 // add middlewares here related to projects
+const Project = require('../projects/projects-model')
+
+async function validateProjectId (req, res, next) {
+    try {
+    const project = await Project.get(req.params.id)
+    if (!project) {
+        res.status(404).json({
+            message: 'project not found'
+        })
+    }
+    else {
+        req.project = project
+        next()
+    }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'Problem finding project!'
+        })
+    }
+}
+
+function validateText (req, res, next) {
+    // try {
+    const { name, description, completed } = req.body;
+    if ( !name || !description ||
+         !name.trim() || !description.trim()  
+        ) {
+        res.status(400).json({
+            message: 'missing required text field'
+        })
+        // next()
+    }
+    else {
+        req.name = name.trim()
+        req.description = description.trim()
+        req.completed = completed
+        next()
+    }
+// } catch (err) {
+    // res.status(500).json(err)
+}
+// }
+
+function validateCompleted (req, res, next) {
+    // try {
+    const { name, description, completed } = req.body;
+    if ( name && description &&
+         (completed === true || completed === false)  
+        ) {
+            req.name = name.trim()
+            req.description = description.trim()
+            req.completed = completed
+        next()
+    }
+    else {
+        res.status(400).json({
+            message: 'missing required text field'
+        })
+        // next()
+    }
+// } catch (err) {
+    // res.status(500).json(err)
+}
+
+module.exports = {
+    validateProjectId,
+    validateText,
+    validateCompleted
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,10 @@
 // Write your "projects" router here!
 const express = require('express');
+const {
+    validateProjectId,
+    validateText,
+    validateCompleted
+} = require('../projects/projects-middleware');
 
 const Project = require('./projects-model');
 
@@ -13,11 +18,12 @@ router.get ('/', (req, res, next) => {
         .catch(next)
 });
 
-router.get ('/:id', (req, res, next) => {
+router.get ('/:id', validateProjectId, (req, res, next) => {
     Project.get(req.params.id)
         .then(project => {
             res.status(200).json(project)
         })
+
         // .catch(err => {
         //     res.status(404).json({
         //         message: err.message
@@ -26,7 +32,8 @@ router.get ('/:id', (req, res, next) => {
         .catch(next)
 });
 
-router.post ('/', (req, res, next) => {
+router.post ('/', validateText, (req, res, next) => {
+    // try {    
     Project.insert({
         name: req.body.name,
         description: req.body.description,
@@ -35,15 +42,36 @@ router.post ('/', (req, res, next) => {
         .then(newProjects => {
             res.status(201).json(newProjects)
         })
+    // }   
         .catch(next)
 });
 
-router.put (() => {
-
+router.put ('/:id', validateCompleted, (req, res, next) => {
+    Project.update(req.params.id, {
+        description: req.body.description,
+        name: req.body.name,
+        completed: req.body.completed
+    })
+        .then(updated => {
+            res.status(200).json(updated)
+        })
+        .catch(next)
 });
 
-// router.delete () => {};
+router.delete ('/:id', validateProjectId, (req, res, next) => {
+    Project.remove(req.params.id)
+        .then(deletedProject => {
+            res.status(200).json(deletedProject)
+        })
+        .catch(next)
+});
 
-// router.get () => {};
+router.get ('/:id/actions', (req, res, next) => {
+    Project.getProjectActions(req.params.id)
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(next)
+});
 
 module.exports = router;
